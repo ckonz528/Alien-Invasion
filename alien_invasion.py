0a8e8702572789173d55c18c8e483d6ac27b1e1a
Raghav Malik <malik22@purdue.edu>
2020-05-11 14:08:02 -0400

    commented out print statement, added gitignore

--- alien_invasion.py
+++ alien_invasion.py
@@ -154,7 +154,7 @@ class AlienInvasion:
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien"""
-        print(self.stats.ships_left)
+        #print(self.stats.ships_left)
         if self.stats.ships_left > 0:
             # Decrement ships_left
             self.stats.ships_left -= 1
